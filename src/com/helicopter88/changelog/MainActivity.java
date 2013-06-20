9de06e4bd3ffea8673a9712e2362cfec0a042471
helicopter88 <johnlemon.lol@gmail.com>
2013-06-20 22:12:49 +0100

    Add RunAsRoot method and execute it
    Signed-off-by: helicopter88 <johnlemon.lol@gmail.com>

--- src/com/helicopter88/changelog/MainActivity.java
+++ src/com/helicopter88/changelog/MainActivity.java
@@ -3,6 +3,7 @@ package com.helicopter88.changelog;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.DataOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -18,21 +19,21 @@ public class MainActivity extends Activity {
     private ListView lvItem;
     private ArrayList<String> itemArray;
     private ArrayAdapter<String> itemAdapter;
-    
+
+    private static final String SD_CARD = Environment.getExternalStorageDirectory().getPath();
+
     /** SU related defines						  **/
     private static final String SYSTEM_CHANGELOG_PATH = "/system/etc/changelog.txt";
-    private static final String CHANGELOG_PATH = Environment.getExternalStorageDirectory().getPath() + "changelog.txt";
-    private static final String SU_COMMAND = "cp -f " + SYSTEM_CHANGELOG_PATH + " " + CHANGELOG_PATH;
-    
+    private static final String CHANGELOG_PATH = SD_CARD + "changelog.txt";
+    private static final String CP_COMMAND = "cp -f " + SYSTEM_CHANGELOG_PATH + " " + CHANGELOG_PATH;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         InputStreamReader inputReader = null;
-        
-        /** Let's make sure our file is in the external storage **/
-        List<String> Shell.SU.run(SU_COMMAND);
-        
+
+
         String file = null;
         String[] splitlines = null;
         try {
@@ -40,7 +41,10 @@ public class MainActivity extends Activity {
             itemArray = new ArrayList<String>();
             itemArray.clear();
             itemAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,itemArray);
-            
+
+            /** Let's make sure our file is in the external storage **/
+	    RunAsRoot(CP_COMMAND);
+
             inputReader = new FileReader(CHANGELOG_PATH);
             while ((inputReader.read(tmp)) >= 0) {
                 file = new String(tmp);
@@ -82,17 +86,30 @@ public class MainActivity extends Activity {
 
     }
 
-    
+    public void RunAsRoot(String cmd) throws IOException {
+            Process p = Runtime.getRuntime().exec("su");
+            DataOutputStream os = new DataOutputStream(p.getOutputStream());
+            try {
+                os.writeBytes(cmd+"\n");
+
+                os.writeBytes("exit\n");
+                os.flush();
+            } catch (IOException e) {
+
+            }
+}
+
     public static String formatChangelog(String rawChangelog) {
+
     /*
      * Example: | Commit:e38f4ac | Title:Primary output check for sonification is removed. | By:Vimal Puthanveed | Date: whatever
      */
 
     	final String CHANGELOG_REGEX =
     			"| Commit: (\\S+) " + 			/* group 1: "Commit hash" */
-    			"| Title: (\\S+) "  +    		/* group 2: Title */
-    			"| Author: (\\S+) " +   		/* group 3: "#1" */
-    			"| Date: (\\S+) ";  			/* group 4: "Date" */
+    			"| Title: (\\S+) "  +    		/* group 2: Title         */
+    			"| Author: (\\S+) " +   		/* group 3: "#1"          */
+    			"| Date: (\\S+) ";  			/* group 4: "Date"        */
 
     	Matcher m = Pattern.compile(CHANGELOG_REGEX).matcher(rawChangelog);
     	if (!m.matches()) {
@@ -100,8 +117,8 @@ public class MainActivity extends Activity {
     	} else if (m.groupCount() < 5) {
     		return "Unavailable";
     	}
-    	return  "Commit" + m.group(1) + "\n" +                 // Commit: e38f4ac
-    			m.group(2) + "\n" +                            // Primary output check for sonification is removed.
-    			" by" + m.group(3) + "on" + m.group(4);        // By Vimal Puthanveed on $date
+    	return  "Commit" + m.group(1) + "\n" +                         // Commit: e38f4ac
+    			   m.group(2) + "\n" +                         // Primary output check for sonification is removed.
+    			   " by" + m.group(3) + "on" + m.group(4);     // By Vimal Puthanveed on $date
     }
 }
