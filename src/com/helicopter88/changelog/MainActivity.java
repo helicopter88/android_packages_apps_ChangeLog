f29fe3a492a90d705e9d6f042c6c5e9321d48e72
Domenico Marino <johnlemon.lol@gmail.com>
2013-06-21 16:39:29 +0200

    Small clean up and reorganization

--- src/com/helicopter88/changelog/MainActivity.java
+++ src/com/helicopter88/changelog/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.Menu;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TabHost;
