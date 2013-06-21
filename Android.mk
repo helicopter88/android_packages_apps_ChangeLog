db99fdf81ea4c23ed3153641f182b68a16c58a77
Domenico Marino <johnlemon.lol@gmail.com>
2013-06-21 17:24:27 +0200

    Remove unneeded bits

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_STATIC_JAVA_LIBRARIES := guava android-support-v4 jsr305
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 
 LOCAL_MODULE_TAGS := optional
 
